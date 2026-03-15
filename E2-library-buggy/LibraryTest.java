e40ae75a4762971bc675bfc9928f657345c6a4de
Lukiluk028 <lucasbarreronadales@gmail.com>
2026-03-15 17:39:03 +0100

    test: añadir pruebas para detectar libro duplicado

---
+++ E2-library-buggy/LibraryTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    @Test
+    public void testAddDuplicateBook() {
+        Library lib = new Library();
+
+        Book b1 = new Book("Titulo", "Autor", "ISBN-123");
+        Book b2 = new Book("Titulo", "Autor", "ISBN-123");
+
+        lib.addBook(b1);
+        lib.addBook(b2);
+
+        assertEquals(1, lib.getBooks().size());
+    }
+}
